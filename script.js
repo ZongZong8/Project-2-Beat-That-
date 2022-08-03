d896b68d8c477d1353e30fb6d08d0d0d64da112c
ZongZong8 <liu_zongli@hotmail.com>
2022-08-03 23:47:20 +0800

    vers 2. two players roll and order dice

--- script.js
+++ script.js
@@ -7,14 +7,20 @@
 //======Problem breakdown and planning ====
 // ver 1 : roll 2 dices and output the result for 1 player. Player chooses dice order.
 // ver 2 : refractor code to include player 2
+//        -global variables for current player; all players scores
+//        -refractor out message to interact with each player 1 or 2
+//        -write logic for player 1 to go first then player 2 and finally compare scores
 // ver 3 : implement comparing dice scores and declare winner.
 // ver 4 : reset the same game so that the players can play continually without refreshing the browser page
 
 var GAME_STATE_DICE_ROLL = "GAME_STATE_DICE_ROLL";
 var GAME_STATE_CHOOSE_DICE_ORDER = "GAME_STATE_CHOOSE_DICE_ORDER";
+var GAME_STATE_COMPARE_SCORES = "GAME_STATE_COMPARE_SCORES";
 var gameState = GAME_STATE_DICE_ROLL;
 
-var playerRolls = [];
+var currentPlayerRolls = [];
+var currentPlayer = 1;
+var allPlayerScore = [];
 
 var rollDice = function () {
   // Generate a decimal from 0 through 6, inclusive of 0 and exclusive of 6.
@@ -33,38 +39,49 @@ var rollDice = function () {
 var rollDiceForPlayer = function () {
   var counter = 0;
   while (counter < 2) {
-    playerRolls.push(rollDice());
+    currentPlayerRolls.push(rollDice());
     counter = counter + 1;
   }
   outputMessage1 =
-    "Welcome! <br><br> You rolled <br>Dice One: " +
-    playerRolls[0] +
+    "Welcome Player " +
+    currentPlayer +
+    "! <br><br> You rolled <br>Dice One: " +
+    currentPlayerRolls[0] +
     " | Dice Two: " +
-    playerRolls[1] +
+    currentPlayerRolls[1] +
     "<br><br> Now please input either '1' or '2' to choose the corresponding dice to be used as the first digit of your final value.";
 
   return outputMessage1;
 };
 
 var getPlayerScore = function (playerInput) {
+  var playerScore = "";
   //input validation (either 1 or 2 only)
   if (playerInput != 1 || playerInput != 2) {
     outputMessage2 =
       "Error! Please key in '1' or '2' only. <br><br>Your dicerolls are <br>Dice One: " +
-      playerRolls[0] +
+      currentPlayerRolls[0] +
       " | Dice Two: " +
-      playerRolls[1] +
+      currentPlayerRolls[1] +
       ".";
   }
   if (playerInput == 1) {
-    var playerScore1 = Number(String(playerRolls[0]) + String(playerRolls[1]));
-    outputMessage2 = "Your chosen value is: " + playerScore1 + ".";
+    playerScore = Number(
+      String(currentPlayerRolls[0]) + String(currentPlayerRolls[1])
+    );
+    outputMessage2 = "Your chosen value is: " + playerScore + ".";
   }
 
   if (playerInput == 2) {
-    var playerScore2 = Number(String(playerRolls[1]) + String(playerRolls[0]));
-    outputMessage2 = "Your chosen value is: " + playerScore2 + ".";
+    var playerScore = Number(
+      String(currentPlayerRolls[1]) + String(currentPlayerRolls[0])
+    );
+    outputMessage2 = "Your chosen value is: " + playerScore + ".";
   }
+
+  allPlayerScore.push(playerScore);
+  currentPlayerRolls = [];
+
   return outputMessage2;
 };
 
@@ -78,7 +95,19 @@ var main = function (input) {
 
   if (gameState == GAME_STATE_CHOOSE_DICE_ORDER) {
     myOutputValue = getPlayerScore(input);
-    return myOutputValue;
+  }
+
+  if (currentPlayer == 1 && gameState == GAME_STATE_CHOOSE_DICE_ORDER) {
+    currentPlayer = 2;
+    gameState = GAME_STATE_DICE_ROLL;
+    return (
+      myOutputValue + "<br></br>It is now Player 2's turn.<br><br>Press submit."
+    );
+  }
+
+  if (currentPlayer == 2 && gameState == GAME_STATE_CHOOSE_DICE_ORDER) {
+    gameState = GAME_STATE_COMPARE_SCORES;
+    return myOutputValue + "<br></br>Press submit to compare scores!";
   }
   return myOutputValue;
 };
